920cd88db43db88504cf2bea8b70bd1b2f678d53
Jacques Soghomonyan <jacques.soghomonyan@efrei.net>
2023-06-10 23:50:03 +0200

    finish up

--- js/form.js
+++ js/form.js
@@ -1,29 +1,32 @@
 console.log("bite")
 
-const NB_OF_QUESTIONS = 1
+const NB_OF_QUESTIONS = 5
 
 function getScore() {
     let form = {
-        "pseudo" : document.getElementById("pseudo").value,
+        // "pseudo" : document.getElementById("pseudo").value,
     }
 
-    // if (form.pseudo === "") {
-    //     alert("Mettez un pseudo")
-    //     return
-    // }
+    // if (form.pseudo === "") return
 
     let score = 0
 
+    let allFiled = true
+
     for (let i = 1; i <= NB_OF_QUESTIONS; i++) {
         const el = document.getElementById("q" + i.toString()).value
+        
+        if (el === "") {
+            allFiled = false
+            break
+        }
+
         form["q" + i.toString()] = el
 
-        if (parseInt(el)) {
-            score++;
-        }
+        if (parseInt(el)) score++
     }
 
-    
-
-    alert(`Vous avez eu ${score} points`)
+    if (allFiled) {
+        alert(`Vous avez eu ${score} points`)
+    }
 }
