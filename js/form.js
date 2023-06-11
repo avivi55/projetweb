aa29f4504f2cae1ba44bad1ee4f4d8cf60b47aa9
Jacques Soghomonyan <jacques.soghomonyan@efrei.net>
2023-06-11 18:21:05 +0200

    📂 file reorganisation + W3C validation

--- js/form.js
+++ js/form.js
@@ -1,16 +1,7 @@
-console.log("bite")
-
 const NB_OF_QUESTIONS = 5
 
 function getScore() {
-    let form = {
-        // "pseudo" : document.getElementById("pseudo").value,
-    }
-
-    // if (form.pseudo === "") return
-
     let score = 0
-
     let allFiled = true
 
     for (let i = 1; i <= NB_OF_QUESTIONS; i++) {
@@ -21,12 +12,8 @@ function getScore() {
             break
         }
 
-        form["q" + i.toString()] = el
-
-        if (parseInt(el)) score++
+        if (parseInt(el)) score++;
     }
 
-    if (allFiled) {
-        alert(`Vous avez eu ${score} points`)
-    }
+    if (allFiled) alert(`Vous avez eu ${score} points`);
 }
